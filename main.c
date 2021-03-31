34ed5cbc79eaad404ca0a847b497b851412a5209
Khazratkulov_Z <khazratkulov.zt@phystech.edu>
2021-03-31 06:23:56 +0300

    some changes in tests

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void test_mem() {
 	byte b0 = 0x0a;
 	b_write(2, b0);
 	byte bres = b_read(2);
+    printf("%02hhx = %02hhx\n", b0, bres);
 	assert(b0 == bres);
 	Address a = 4;
 	byte b1 = 0xcb;
@@ -17,10 +18,12 @@ void test_mem() {
 	b_write(a, b0);
 	b_write(a+1, b1);
 	word wres = w_read(a);
+    printf("ww/br \t %04hx=%02hhx%02hhx\n", wres, b1, b0);
 	assert(w == wres);
 	w = 0x0a0b;
 	w_write(a, w);
 	wres = w_read(a);
+    printf("%02hx=%02hx\n", wres, w);
 	assert(w == wres);
 }
 
@@ -46,4 +49,3 @@ void w_write(Address adr, word b)
     mem[adr] = b;
     mem[adr + 1] = b >> 8;
 }
-
