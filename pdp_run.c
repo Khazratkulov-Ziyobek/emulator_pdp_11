43573da2c36a67bd05283ade07539b4f82d7d391
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-24 18:21:04 +0300

    some definitions in get_mr

--- pdp_run.c
+++ pdp_run.c
@@ -11,6 +11,14 @@ Argument get_mr(word w)
     Argument res;
     int r = w & 7; // номер регистра
     int mode = (w >> 3) & 7; // номер мода
+    switch(mode)
+    {
+        case 0:     // mode 0
+            res.adr = r;
+            res.val  = reg[r];
+            trace("R%o ", r);
+            break;
+    }
     return res;
 }
 
