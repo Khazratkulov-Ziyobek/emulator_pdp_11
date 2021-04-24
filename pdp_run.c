69e61ca1fefda9668d01998c20e2ced34b12fd5c
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-24 18:25:39 +0300

    added mode 1 in get_mr

--- pdp_run.c
+++ pdp_run.c
@@ -13,10 +13,15 @@ Argument get_mr(word w)
     int mode = (w >> 3) & 7; // номер мода
     switch(mode)
     {
-        case 0:     // mode 0
+        case 0:     // mode 0 R3
             res.adr = r;
             res.val  = reg[r];
             trace("R%o ", r);
+            break;
+        case 1:     // mode 1 (R3)
+            res.adr = reg[r];
+            res.val  = w_read(res.adr); //b_read тоже нудно написать
+            trace("(R%o) ", r);
             break;
         
     }
