f7844383d4bce055add04061102410939893da5e
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-25 00:04:54 +0300

    correct some mistakes

--- commands.c
+++ commands.c
@@ -4,23 +4,23 @@
 
 void print_reg()
 {
-    trace("R0:%o R1:%o R2:%o R3:%o R4:%o R5:%o R6:%o R7:%o\n", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
+    trace("R0:%o R1:%o R2:%o R3:%o\n R4:%o R5:%o R6:%o R7:%o\n", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
 }
 
-void do_halt()
+void do_halt(Argument ss, Argument dd)
 {
     trace("THE END!!!\n");
     print_reg();
     exit(0);
 }
 
-void do_mov() {
+void do_mov(Argument ss, Argument dd) {
     w_write(dd.adr, ss.val);
 }
-void do_add() {
+void do_add(Argument ss, Argument dd) {
     reg[dd.adr] = reg[dd.adr] + reg[ss.adr];
 }
-void do_nothing() 
+void do_nothing(Argument ss, Argument dd) 
 {
     trace("UNKNOWN FUNCTION\n");
     exit(0);
