a0e26d1e1368ecaf71dd87f63db88d9b932249fb
Ziyobek <khazratkulov.zt@phystech.edu>
2021-04-25 04:29:37 +0300

    some addition to do_halt

--- commands.c
+++ commands.c
@@ -4,12 +4,12 @@
 
 void print_reg()
 {
-    trace("R0:%o R1:%o R2:%o R3:%o\nR4:%o R5:%o R6:%o R7:%o\n", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
+    trace("R0:%06o R1:%06o R2:%06o R3:%06o\nR4:%06o R5:%06o R6:%06o R7:%06o\n", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
 }
 
 void do_halt(Argument ss, Argument dd)
 {
-    trace("THE END!!!\n");
+    trace("---------------- halted ---------------\n");
     print_reg();
     exit(0);
 }
