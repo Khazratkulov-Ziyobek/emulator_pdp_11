cdbd27894e635544c0d7d24e5f8247d524a9c3fb
Ziyobek <khazratkulov.zt@phystech.edu>
2021-05-11 13:59:10 +0300

    modified mode 1, 2 and added mode 3

--- pdp_run.c
+++ pdp_run.c
@@ -19,20 +19,49 @@ Argument get_mr(word w)
             break;
         case 1:     // mode 1 (R3)
             res.adr = reg[r];
-            res.val  = w_read(res.adr); //b_read тоже нудно написать
+            if( wb == 0 || r == 7 || r == 6)
+            {
+                res.val  = w_read(res.adr);
+            }
+            else
+            {
+                res.val = b_read(res.adr);
+            }      
             trace("(R%o) ", r);
             break;
         case 2:     // mode 2 (R3)+ #3
             res.adr = reg[r];
-            res.val  = w_read(res.adr); //to do b_read
-            reg[r] += 2; //to do +1
-            if(r == 7)
+            if (wb == 0 || r == 6)
             {
+                res.val  = w_read(res.adr);
+                reg[r] += 2;
+                trace("(R%o)+ ", r);
+            }
+            else if(r == 7)
+            {
+                res.val  = w_read(res.adr);
+                reg[r] += 2;
                 trace("#%o ", res.val);
             }
             else
             {
+                res.val = b_read(res.adr);
+                reg[r] += 1;
                 trace("(R%o)+ ", r);
+            }
+            break;
+        case 3:         //mode 3 @(R3)+ 
+            res.adr = reg[r];
+            res.adr = w_read(res.adr);
+            res.val = w_read(res.adr);
+            reg[r] += 2;
+            if(r == 7) 
+            {
+                trace("@#%o ", res.adr);
+            }
+            else 
+            {
+                trace("@(R%o)+ ", r);
             }
             break;
         default:
@@ -60,6 +89,14 @@ void run()
             {
                 trace("%s \n", cmd[i].name);
                 cmd[i].do_func(ss, dd);
+                if(cmd[i].params == HAS_SS)
+                {
+                    ss = get_mr(w >> 6);
+                }
+                if(cmd[i].params == HAS_DD)
+                {
+                    dd = get_mr(w);
+                }
                 // trace("\n");
                 break;
             }
