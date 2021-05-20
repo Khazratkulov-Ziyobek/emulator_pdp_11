1db31cee116496ba945523cd9a2d11cb5eb5e49e
Ziyobek <khazratkulov.zt@phystech.edu>
2021-05-20 10:57:11 +0500

    corrected load_func and added new functions

--- main_function.c
+++ main_function.c
@@ -2,9 +2,11 @@
 #include <assert.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pdp_11.h"
 
 
+int check = 0;
 
 void b_write(Address adr, byte b)
 {
@@ -29,9 +31,22 @@ void w_write(Address adr, word b)
     mem[adr + 1] = b >> 8;
 }
 
-void load_file(const char * filename)
+void load_file(int argc, char  * argv[])
 {
-    FILE *fin = fopen(filename, "r"); 
+    FILE *fin = fopen(argv[argc - 1], "r");
+    if(strcmp(argv[argc - 1], "-t") == 0) {
+        usage_print();
+        exit(0);
+    }
+    else if(strcmp(argv[argc - 1], "-T") == 0) {
+        usage_print();
+        exit(0);
+    }
+    else if(fin == NULL) {
+        printf("can't open %s for reading\n", argv[argc - 1]);
+		exit(0);
+    }
+        
     unsigned short int adr_start;
     unsigned short int quantity;
     while(fscanf(fin,"%hx%hx", &adr_start, &quantity) == 2)
@@ -54,10 +69,36 @@ void mem_dump(Address adr, word n)
     }
 }
 
+void usage_print() {
+	printf("Usage:\n ./pdp [options] initial-core-file [options to emulated program]\n\n");
+	printf("Options:\n\t-t\tshow trace to stderr\n");
+    printf("\t-T\tshow verbose trace to stderr\n");
+}
+
 void trace(const char *fmt, ...)
 {
-    va_list ap;
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
+    if(check == 1 || check == 2) {
+        va_list ap;
+        va_start(ap, fmt);
+        vprintf(fmt, ap);
+        va_end(ap);
+    }
+}
+
+void check_trace(int argc, char * argv[])
+{
+    for(int i = 0; i < argc; i++)
+    {
+        if(strcmp(argv[i], "-t") == 0)
+            check = 1;
+    }
+}
+
+void check_bigtrace(int argc, char * argv[])
+{
+    for(int i = 0; i < argc; i++)
+    {
+        if(strcmp(argv[i], "-T") == 0)
+            check = 2;
+    }
 }
